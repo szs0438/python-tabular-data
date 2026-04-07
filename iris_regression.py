1c2c07db050445552c22e2c3459fe7d032618cb5
Sharmin Sultana <sharminomi87@gmail.com>
2026-04-07 16:28:08 -0500

    I added regression function and species loop

--- iris_regression.py
+++ iris_regression.py
@@ -5,15 +5,28 @@ Basic iris data loader
 """
 
 import pandas as pd
+from scipy import stats
 
 
 def load_data(filename):
     return pd.read_csv(filename, sep="\t")
 
 
+def run_regression(data):
+    x = data["petal_length_cm"]
+    y = data["sepal_length_cm"]
+    return stats.linregress(x, y)
+
+
 def main():
     df = load_data("iris.csv")
-    print(df.head())
+
+    species_list = df["species"].unique()
+
+    for species in species_list:
+        species_data = df[df["species"] == species]
+        result = run_regression(species_data)
+        print(species, result.slope)
 
 
 if __name__ == "__main__":
